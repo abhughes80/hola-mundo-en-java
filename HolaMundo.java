ca172aea3a8ce4692dc90930d95b2d0a34238717
Ana Belén Alloza Subías <bravesmooth00@gmail.com>
2018-03-23 21:21:55 +0100

    comentarios añadidos al código

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,4 +1,6 @@
-#
+/** Muestra por pantalla la frase Hola Mundo!
+*
+@author Ana Belén Alloza*/
 public static HolaMundo {
   public static void main (String[] args){
     System.out.println("Hola Mundo");
